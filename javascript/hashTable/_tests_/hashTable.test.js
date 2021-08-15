1d562ef965bbe1c97f1d7d97623096c47dd27c81
awwadsaeed <saeedawwad450@gmail.com>
2021-08-15 15:38:56 +0300

    challenge 31

--- javascript/hashTable/_tests_/hashTable.test.js
+++ javascript/hashTable/_tests_/hashTable.test.js
@@ -1,7 +1,7 @@
 const HashTable = require('../hashTable').HashTable;
 const Node = require('../hashTable').Node;
 const LinkedList = require('../hashTable').LinkedList;
-
+const repeatedWord = require('../hashTable').repeatedWord;
 
 describe('HashTable primary Test',()=>{
     let hashy = new HashTable(4000);
@@ -25,5 +25,19 @@ describe('HashTable primary Test',()=>{
     });
     test('can hash stuff',()=>{
         expect(hashy.hash('stuff')).toBe(2488);
+    })
+})
+describe('test repeated word',()=>{
+    test('test for repeated word 1',()=>{
+        let test = '';
+        expect(repeatedWord(test)).toBe('cannot find words in an empty string');
+    })
+    test('test for repeated word 2',()=>{
+        let test = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...";
+        expect(repeatedWord(test)).toBe('it');
+    })
+    test('test for repeated word 3',()=>{
+        let test = 'some string with no repeated words';
+        expect(repeatedWord(test)).toBe('no repeated words appeard');
     })
 })
